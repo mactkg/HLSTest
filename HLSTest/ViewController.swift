ee344879b99860fca4eb93dea18fd3edbfa1e9f6
mactkg <mactkg@gmail.com>
2021-11-13 01:55:12 +0900

    Initial Commit

---
+++ HLSTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HLSTest
+//
+//  Created by Kenta Hara on 2021/11/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
